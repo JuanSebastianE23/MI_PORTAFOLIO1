48109219911a30346f24c96b60aaa4d0dba9ee78
Juan Sebastian Estacio <juan.estacioenri@campusucc.edu.co>
2026-04-21 20:16:41 -0500

    subiendo ultimos cambios al portafolio

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       contacto: 'contacto',
       hola: 'Hola, soy',
       role: 'Desarrollador Full Stack',
-      subtitle: 'Creando experiencias digitales increíbles con pasión y código limpio',
+      subtitle: 'Creando experiences digitales increíbles con pasión y código limpio',
       descargarCV: 'Descargar CV',
       contactar: 'Contactar',
       acercaTitulo: 'Acerca de mí',
